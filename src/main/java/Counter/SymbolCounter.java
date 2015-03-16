7651249a806782edcf87f27d0c5c47be47c7b8ed
Nauris <zippper@inbox.lv>
2015-03-16 14:36:10 +0200

    First commit

---
+++ src/main/java/Counter/SymbolCounter.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+
+
+public class SymbolCounter {
+
+    public static void main(String []args) {
+	InputStream inputStream = null;
+	Properties prop = new Properties();	
+
+	try {
+		inputStream = new FileInputStream(args[0]); 
+		if (inputStream != null){
+			prop.load(inputStream);
+		} else {
+			System.out.println("No data");
+	}	
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	}	
+	
+	System.out.println(prop.getProperty("message"));	
+	}
+
+} 
